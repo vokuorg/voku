4b3d7c2b2388a557e155d7db9d0128d8481f6115
jokenox <jokenox@gmail.com>
2021-06-06 23:26:39 -0500

    Adjust positioning at home page and beautify code

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,46 +1,41 @@
 import React from 'react';
 import Logo from '../logo/Logo';
+import { Link } from "react-router-dom";
 import BalloonButton from '../balloon-button/BalloonButton';
 
 import './NavBar.css';
 import '../../index.css';
 
-function NavBar( {classNames} ) {
-    return (
-        <div className = { `${classNames}` }>
-            <div className = 'flex inline-block'>
-                <div className = 'pt-2 pr-4'>
-                    <Logo textColor = 'white' />
-                </div>
-                <a href="http://localhost:3000/"
-                    className = 'flex-1 inline-block p-4 text-2xl text-gray-light'>
-                    About
-                </a>
-                <a href="http://localhost:3000/"
-                    className = 'flex-1 inline-block p-4 text-2xl text-gray-light'>
-                    Donate
-                </a>
-                <a href="https://github.com/videovoko/videovoko-org"
-                    target = "_blank"
-                    rel = "noopener noreferrer"
-                    className = 'flex-1 inline-block p-4 text-2xl text-gray-light'>
-                    GitHub
-                </a>
-            </div>
-            <div className = 'flex inline-block pr-3 mr-32 space-x-5'>
-                <BalloonButton
-                    classNames = 'bg-trasparent text-white roboto-font text-sm border-4 border-gray h-10 py-1 mt-2'
-                    side = "left"
-                    text = "Sign In"
-                />
-                <BalloonButton
-                    classNames = 'bg-secondary text-white roboto-font text-sm border-secondary h-10 py-1 mt-2'
-                    side = "right"
-                    text = "Sign Up"
-                />
-            </div>
-        </div>
-    );
+function NavBar({ classNames }) {
+  return (
+    <div className={`flex justify-between py-3 ${classNames || ''}`}>
+      <div className="flex space-x-10">
+        <Link to="/">
+          <Logo textColor="white" />
+        </Link>
+
+        <Link to="/call" 
+          className="mt-auto text-2xl align-bottom text-gray-light hover:text-secondary" href="#">
+          About
+        </Link>
+
+        <a className="mt-auto text-2xl text-gray-light hover:text-secondary" href="#">
+          Donate
+        </a>
+        
+        <a className="mt-auto text-2xl text-gray-light hover:text-secondary" href="https://github.com/videovoko/videovoko-org" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </div>
+
+      <div className="space-x-5">
+        <BalloonButton classNames="bg-trasparent text-white font-semibold roboto-font text-sm border-4 border-gray h-10 py-1"
+          side="left" text="Sign In" />
+        <BalloonButton classNames="bg-secondary text-white font-semibold roboto-font text-sm border-secondary h-10 py-1"
+          side="right" text="Sign Up" />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,33 +5,43 @@ import NavBar from '../../components/nav-bar/NavBar';
 
 function Home() {
   return (
-    <div className = 'w-full h-screen overflow-hidden bg-dark'>
-      <div className = 'container flex-row w-full pl-12 mt-8'>
-        <NavBar classNames = 'flex justify-between' />
-      </div>
-      <div className = 'container flex flex-row justify-center pl-12'>
-        <div className = 'flex flex-col justify-center w-1/2 h-screen'>
-          <div className = 'flex flex-col justify-center h-full'> 
-            <h1 className = 'mb-4 text-5xl font-bold leading-snug text-left text-white roboto-font'>Speak Esperanto With Someone Around The World<font className = 'color-primary'>.</font></h1>
-            <p className = 'w-9/12 mb-6 text-2xl leading-normal text-left text-gray-light roboto-font'>Be part of the hub of Esperanto speakers and promote this beautiful language.</p>
-            <div className = 'flex justify-start space-x-5'>
-              <BalloonButton
-                classNames = 'text-3xl text-white roboto-font font-medium bg-esperanto border-esperanto'
-                side = 'left'
-                text = 'Random Call'
-              />
-              <BalloonButton
-                classNames = 'text-3xl text-white roboto-font font-medium bg-dark-tertiary border-dark-tertiary'
-                side = 'right'
-                text = 'Call Friend'
-              />
+    <div className="flex flex-col w-full h-screen px-20 bg-dark">
+      <NavBar classNames="" />
+
+      <div className="grid justify-center h-full grid-cols-2">
+
+        {/* Text and call to action*/}
+        <div className="flex flex-col content-center w-full h-auto col-span-1">
+          
+          <div className="flex flex-col justify-center w-full h-full">
+            <h1 className="mb-3 text-5xl font-bold leading-snug text-left text-white roboto-font">Speak Esperanto With
+              Someone Around The World<font className="text-primary">.</font>
+            </h1>
+            
+            <p className="w-9/12 text-2xl leading-normal text-left mb-7 text-gray-light roboto-font">Be part of the hub of
+              Esperanto speakers and promote this beautiful language.
+            </p>
+
+            <div className="flex justify-start space-x-5">
+              <BalloonButton classNames="text-3xl text-white roboto-font font-medium bg-esperanto border-esperanto"
+                side="left" text="Random Call" />
+                
+              <BalloonButton classNames="text-3xl text-white roboto-font font-medium bg-dark-tertiary border-dark-tertiary"
+                side="right" text="Call Friend" />
             </div>
           </div>
-          <p className = 'mb-8 text-xl leading-normal text-left text-gray-400 roboto-font'>© 2021 videovoko</p>
         </div>
-        <div className = 'flex flex-col justify-center w-1/2 h-screen p-5'>
-          <HomeIllustration className = 'w-full h-full'/>
+
+        {/* Illustration */}
+        <div className="flex justify-center w-full h-full col-span-1">
+          <HomeIllustration className="h-full" />
         </div>
+      </div>
+
+      <div className="py-1">
+        <p className="text-lg text-left text-gray-400 roboto-font">
+          © 2021 videovoko
+        </p>
       </div>
     </div>
   );

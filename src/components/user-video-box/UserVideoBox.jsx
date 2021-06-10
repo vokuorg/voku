590e926aacda44e988c6771d971576395224e3e3
jokenox <jokenox@gmail.com>
2021-06-09 23:02:36 -0500

    Add UserVideoBox state handler

--- src/components/user-video-box/UserVideoBox.jsx
+++ src/components/user-video-box/UserVideoBox.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../index.css';
 import './UserVideoBox.css';
 
-function UserVideoBox({ user, name, color }) {
+function UserVideoBox({ user, name, color, onClickVideo }) {
+  const [playing, setPlaying] = useState(false);
+
+  const handlePlay = e => {
+    setPlaying(true);
+  };
+
   return (
-    <div className="flex flex-wrap content-center justify-center border-2 border-solid rounded-lg bg-dark-secondary border-gray video-container">
-      <div className={`${color || ''} flex flex-wrap content-center justify-center w-1/3 text-5xl text-white rounded-full h-1/3`}>
+    <div className="relative flex flex-wrap content-center justify-center border-2 border-solid rounded-lg bg-dark-secondary border-gray medium-size">
+      <div className={playing ? 'hidden' : `${color || ''} flex flex-wrap content-center justify-center w-24 h-24 my-auto text-5xl text-white rounded-full`}>
         { name[0] }
       </div>
-      <video id={`${user}-video`} className="hidden"></video>
+
+      <video 
+        id={`${user}-video`}
+        className={playing ? 'w-full h-full bg-black rounded-lg' : 'hidden'}
+        onClick={onClickVideo}
+        onPlay={handlePlay}
+      ></video>
+
+      <div className="absolute bottom-0 left-0 px-2 py-1 text-white rounded-tr-lg rounded-bl-md bg-dark-tertiary">
+        {name}
+      </div>
     </div>
   );
 }

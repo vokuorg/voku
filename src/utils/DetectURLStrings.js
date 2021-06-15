97bd6b571d2ce66251529c3981970e823f9b970d
Felipe Saul Cabrera Zaragoza <saul_quantumtech@outlook.com>
2021-06-15 13:09:28 -0500

    Add {PlainTextLinks_To_HTMLLinks(String): String} function that returns a plain text with links formated in HMTL <a></a>. /src/utils/DetectURLStrings.js

--- src/utils/DetectURLStrings.js
+++ src/utils/DetectURLStrings.js
@@ -1,8 +1,8 @@
 
-//Devuelve una lista de objetos URL que indican los URL encontrados en un texto
-function DetectURLStrings(text) {
+//Retorna una lista de objetos URL que indican los URL encontrados en un texto
+function DetectURLStrings(text = '') {
     class URL {
-        constructor (str, isLink) {
+        constructor (str = '', isLink = false) {
             this.str = str;
             this.isLink = isLink;
         }
@@ -24,12 +24,12 @@ function DetectURLStrings(text) {
     //Expresion regular para identificar URLs
     const regEx = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi;
 
-    for(let i = 0; i < lines.length; i++) {
+    for (let i = 0; i < lines.length; i++) {
         //Dividir cada linea por palabras en un arreglo de palabras
         wordsArr = lines[i].split(' ');
 
-        for(let j = 0; j < wordsArr.length; j++){
-            if(regEx.test(wordsArr[j])){
+        for (let j = 0; j < wordsArr.length; j++){
+            if (regEx.test(wordsArr[j])){
                 var url = new URL(wordsArr[j], true);
                 linksFound.push(url);
             }
@@ -38,3 +38,24 @@ function DetectURLStrings(text) {
     
     return linksFound;
 }
+
+//Retorna un texto con los links formateados con la etiqueta HTML <a></a>
+function PlainTextLinks_To_HTMLLinks(text = ''){
+    let links = [];
+
+    links = DetectURLStrings(text);
+
+    let newText = text;
+
+    for (let i = 0; i < links.length; i++){
+        newText = newText.replace(links[i].String, '<a href="' + links[i].String + '">' + links[i].String + '</a>');
+    }
+
+    return newText;
+}
+
+// * Se puede obtener el texto de un mensaje de chat mediante su ID
+// * const text = document.getElementById('text').textContent;
+
+// * Para reemplazar el texto del mensaje de chat por el texto nuevo se usa innerHTML
+// * document.getElementById('text').innerHTML=(PlainTextLinks_To_HTMLLinks(text));

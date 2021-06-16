0a3e8d88ddb4c06f73a06ff4c9ad2e8aaed493a4
jokenox <jokenox@gmail.com>
2021-06-15 20:11:07 -0500

    Replace the state from UserVideoBox by a context

--- src/components/user-video-box/UserVideoBox.jsx
+++ src/components/user-video-box/UserVideoBox.jsx
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useContext } from 'react';
+import { MediaContext } from '../../contexts/Media/MediaContext';
 
 import '../../index.css';
 import './UserVideoBox.css';
 
 function UserVideoBox({ user, name, color, onClickVideo }) {
-  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
 
-  const handlePlay = e => {
-    setPlaying(true);
-  };
+  const {
+    localStream,
+    guestStream,
+    localMediaStatus,
+    guestMediaStatus
+  } = useContext(MediaContext);
+
+  const stream = user === 'local' ? localStream : guestStream;
+  const mediaStatus = user === 'local' ? localMediaStatus : guestMediaStatus;
+
+  useEffect(() => {
+    let video = videoRef.current;
+    
+    if (!video.srcObject && stream) {
+      video.srcObject = stream;
+      video.play();
+    }
+  });
 
   return (
     <div className="relative flex flex-wrap content-center justify-center border-2 border-solid rounded-lg bg-dark-secondary border-gray medium-size">
-      <div className={playing ? 'hidden' : `${color || ''} flex flex-wrap content-center justify-center w-24 h-24 my-auto text-5xl text-white rounded-full`}>
-        { name[0] }
+      <div className={mediaStatus.video ? 'hidden' : `${color || ''} flex flex-wrap content-center justify-center w-24 h-24 my-auto text-5xl text-white rounded-full`}>
+        {name[0]}
       </div>
 
-      <video 
+      <video
         id={`${user}-video`}
-        className={playing ? 'w-full h-full bg-black rounded-lg' : 'hidden'}
+        ref={videoRef}
+        className={
+          mediaStatus.video
+          ? (`w-full h-full bg-black rounded-lg ${user === 'local' ? 'mirrored' : ''}`)
+          : 'hidden'
+        }
         onClick={onClickVideo}
-        onPlay={handlePlay}
+        muted={user === 'local'}
       ></video>
 
       <div className="absolute bottom-0 left-0 px-2 py-1 text-white rounded-tr-lg rounded-bl-md bg-dark-tertiary">

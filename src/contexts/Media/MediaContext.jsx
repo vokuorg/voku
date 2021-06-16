d24cbcb90be16d663f9bdb0b0e763033ab7301ac
jokenox <jokenox@gmail.com>
2021-06-15 20:00:54 -0500

    Add Media Context

---
+++ src/contexts/Media/MediaContext.jsx
@@ -0,0 +1,88 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+const MediaContext = createContext();
+
+const MediaState = ({ children }) => {
+  const [localStream, setLocalStream] = useState();
+  const [guestStream, setGuestStream] = useState();
+  const [localMediaStatus, setLocalMediaStatus] = useState({video: false, audio: false });
+  const [guestMediaStatus, setGuestMediaStatus] = useState({ video: false, audio: false });
+
+  useEffect(() => {
+    getLocalStream();
+  });
+  
+  const getLocalStream = () => {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(stream => {
+      setLocalStream(stream);
+      setLocalMediaStatus({ video: true, audio: true });
+      //playLocalVideo();
+    }).catch(err => console.log(err));
+  }
+  
+  const enableLocalVideo = () => {
+    localStream.getVideoTracks()[0].enabled = true;
+    setLocalMediaStatus(prevStatus => ({ ...prevStatus, video: true }));
+  };
+
+  const enableGuestVideo = () => {
+    setGuestMediaStatus(prevStatus => ({ ...prevStatus, video: true }));
+  };
+
+  
+  const enableLocalAudio = () => {
+    localStream.getAudioTracks()[0].enabled = true;
+    setLocalMediaStatus(prevStatus => ({ ...prevStatus, audio: true }));
+  }
+  
+  const enableGuestAudio = () => {
+    setGuestMediaStatus(prevStatus => ({ ...prevStatus, audio: true }));
+  }
+  
+  
+  const disableLocalVideo = () => {
+    localStream.getVideoTracks()[0].enabled = false;
+    setLocalMediaStatus(prevStatus => ({ ...prevStatus, video: false }));
+  };
+
+  const disableGuestVideo = () => {
+    setGuestMediaStatus(prevStatus => ({ ...prevStatus, video: false }));
+  };
+
+  
+  const disableLocalAudio = () => {
+    localStream.getAudioTracks()[0].enabled = false;
+    setLocalMediaStatus(prevStatus => ({ ...prevStatus, audio: false }));
+  };
+  
+  const disableGuestAudio = () => {
+    setGuestMediaStatus(prevStatus => ({ ...prevStatus, audio: false }));
+  };
+
+
+  return (
+    <MediaContext.Provider value={{
+      getLocalStream,
+      setGuestStream,
+      localStream,
+      guestStream,
+      localMediaStatus,
+      guestMediaStatus,
+      enableLocalVideo,
+      enableGuestVideo,
+      enableLocalAudio,
+      enableGuestAudio,
+      disableLocalVideo,
+      disableGuestVideo,
+      disableLocalAudio,
+      disableGuestAudio
+    }}>
+      { children }
+    </MediaContext.Provider>
+  );
+};
+
+export { MediaContext, MediaState };
--- src/pages/CallRoom/CallRoom.jsx
+++ src/pages/CallRoom/CallRoom.jsx
@@ -5,38 +5,42 @@ import Chat from '../../components/chat/Chat';
 import UserVideoBox from '../../components/user-video-box/UserVideoBox';
 import CallControls from '../../components/call-controls/CallControls';
 
+import { MediaState } from '../../contexts/Media/MediaContext';
+
 function CallRoom() {
   return (
-    <div className="grid w-full h-screen grid-cols-8 overflow-hidden bg-dark">
-      <div className="flex flex-col h-full col-span-6 main-panel">
-        <div className="py-3">
-          <Logo textColor="white" />
+    <MediaState>
+      <div className="grid w-full h-screen grid-cols-8 overflow-hidden bg-dark">
+        <div className="flex flex-col h-full col-span-6 main-panel">
+          <div className="py-3">
+            <Logo textColor="white" />
+          </div>
+
+          <div className="flex flex-wrap content-center justify-center w-full h-full space-x-8 video-area">
+            <UserVideoBox
+              user="local"
+              name="Kenny"
+              color="bg-indigo-800"
+            />
+
+            <UserVideoBox 
+              user="remote"
+              name="Jokenox"
+              color="bg-blue-800"
+            />
+          </div>
+
+          <div className="py-3">
+            <CallControls />
+          </div>
         </div>
 
-        <div className="flex flex-wrap content-center justify-center w-full h-full space-x-8 video-area">
-          <UserVideoBox
-            user="local"
-            name="Kenny"
-            color="bg-indigo-800"
-          />
-
-          <UserVideoBox 
-            user="remote"
-            name="Jokenox"
-            color="bg-blue-800"
-          />
-        </div>
-
-        <div className="py-3">
-          <CallControls />
+        <div className="h-screen col-span-2 text-white rounded-l-lg bg-dark-tertiary side-panel">
+          <Chat />
+          <span></span>
         </div>
       </div>
-
-      <div className="h-screen col-span-2 text-white rounded-l-lg bg-dark-tertiary side-panel">
-        <Chat />
-        <span></span>
-      </div>
-    </div>
+    </MediaState>
   );
 }
 

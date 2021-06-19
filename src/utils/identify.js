12deb1a177128abf28ae1c020058eaa22c4501a7
jokenox <jokenox@gmail.com>
2021-06-19 02:23:29 -0500

    Add identify.js to utils

---
+++ src/utils/identify.js
@@ -0,0 +1,21 @@
+const generateId = () => {
+  let abc = 'abcdefghijklmnopqrstuvwxyz';
+  let id = '';
+  
+  for (let i = 0; i < 9; i++) {
+    if (i === 4) id += '-';
+    else id += abc[Math.ceil(Math.random() * 26) - 1];
+  }
+
+  return id;
+};
+
+const setRoomId = (id) => {
+  window.location.hash = id;
+}
+
+const getRoomId = () => {
+  return window.location.hash.substr(1);
+}
+
+export { generateId, setRoomId, getRoomId };

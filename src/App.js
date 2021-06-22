a3145ef7ec2923e1eb889df116bb648cb6c22e54
jokenox <jokenox@gmail.com>
2021-06-21 20:14:57 -0500

    Add AppContextProvider and HashRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 import {
-  BrowserRouter,
+  HashRouter,
   Switch,
   Route
 } from "react-router-dom";
+
+import { AppContextProvider } from './contexts/AppContext/AppContext';
 
 import Home from './pages/Home/Home';
 import CallRoom from './pages/CallRoom/CallRoom';
@@ -12,17 +14,29 @@ import CallRoom from './pages/CallRoom/CallRoom';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/call">
-            <CallRoom />
-          </Route>
+      <AppContextProvider>
 
-          <Route path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+        <HashRouter hashType={ 'noslash' }>
+          <Switch>
+            <Route
+              exact
+              path="/:id"
+              render={({ match }) => {
+                if (!/^[a-z]{4}-[a-z]{4}$/.test(match.params.id)) {
+                  return <Home />;
+                }
+                
+                return <CallRoom />;
+              }}
+            />
+
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </HashRouter>
+        
+      </AppContextProvider>
     </div>
   );
 }

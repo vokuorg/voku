48afec3eba52c836a767ee72209dc6cb129525c4
jokenox <jokenox@gmail.com>
2021-06-22 11:23:59 -0500

    Add hover effects to buttons in call room

--- src/components/call-controls/CallControls.jsx
+++ src/components/call-controls/CallControls.jsx
@@ -40,7 +40,7 @@ function CallControls() {
   return (
     <div className="relative flex content-center justify-center space-x-3">
       { /* Volume Button */ }
-      <Button classNames="h-9 my-auto bg-dark-tertiary text-white absolute left-3">
+      <Button classNames="h-9 my-auto bg-dark-tertiary hover:bg-gray text-white absolute left-3">
         <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z" clipRule="evenodd" />
         </svg>
@@ -56,7 +56,7 @@ function CallControls() {
         onClick={handleAudioButton}
         classNames={
           `${localMediaStatus.audio ? 'text-white' : 'text-gray-500'}
-          h-9 my-auto bg-dark-tertiary hover:bg-dark-tertiary`
+          h-9 my-auto bg-dark-tertiary hover:bg-gray`
         }
       >
         <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
@@ -81,7 +81,7 @@ function CallControls() {
         onClick={handleVideoButton}
         classNames={
           `${localMediaStatus.video ? 'text-white' : 'text-gray-500'}
-          h-9 my-auto bg-dark-tertiary hover:bg-dark-tertiary`
+          h-9 my-auto bg-dark-tertiary hover:bg-gray`
         }
       >
         <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
@@ -91,7 +91,7 @@ function CallControls() {
 
 
       { /* Expand Button */ }
-      <Button classNames="h-9 my-auto bg-dark-tertiary hover:bg-dark-tertiary text-white absolute right-3">
+      <Button classNames="h-9 my-auto bg-dark-tertiary hover:bg-gray text-white absolute right-3">
         <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
         </svg>

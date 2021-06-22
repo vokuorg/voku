ff2a6e8e28828df8e18b03c88b6bb47637e85371
jokenox <jokenox@gmail.com>
2021-06-22 06:28:28 -0500

    Add some properties to Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 import '../../index.css';
 import './Button.css';
 
-function Button({ type, onClick, classNames, children }) {
+function Button({ type, onClick, classNames, children, disabled }) {
   return (
-    <button type={type} onClick={onClick} className={`inline-flex items-center px-2 py-1 rounded-lg focus:outline-none ${classNames || ''}`}>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`inline-flex items-center px-2 py-1 rounded-lg focus:outline-none disabled:opacity-50 ${classNames || ''}`}
+      disabled={ disabled }
+      >
       { children }
     </button>
   );

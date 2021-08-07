33044f166c6b80b2d8bd6fe46463e0af7fd7216d
jokenox <jokenox@gmail.com>
2021-08-07 04:09:00 -0500

    Add UserInfoModal

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import '../../index.css';
 import './Modal.css';
 
-function Modal({ id, title, ref, children }) {
-  const [visibility, setVisibility] = useState(false);
+function Modal({ id, title, ref, children, initialVisibility = false }) {
+  const [visibility, setVisibility] = useState(initialVisibility);
 
   const handleVisibility = (e) => {
     if (e.target.id === id) {
@@ -18,18 +18,21 @@ function Modal({ id, title, ref, children }) {
   
   return (
     <div
-    id={id}
-    ref={ref}
-    onClick={handleVisibility}
-    className={`${!visibility ? 'hidden' : ''} fixed inset-0 inset-x-0 z-50 flex items-center w-full h-full bg-black bg-opacity-50`}
+    id={ id }
+    ref={ ref }
+    onClick={ handleVisibility }
+    className={`
+      ${ !visibility ? 'hidden' : '' }
+      fixed inset-0 inset-x-0 z-50 flex items-center w-full h-full bg-black bg-opacity-50
+    `}
     >
 
-      <div className="w-1/3 mx-auto text-white rounded-lg bg-dark-tertiary">
-        <div className="flex justify-between px-5 py-3 mb-5 text-2xl">
+      <div className="w-5/6 mx-auto text-white rounded-lg md:w-1/3 bg-dark-tertiary">
+        <div className="flex justify-between px-5 py-3 mb-5 text-4xl md:text-2xl">
           { title }
 
           <button onClick={hideModal} className="focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 md:w-6 md:h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
---
+++ src/components/modals/UserInfoModal.jsx
@@ -0,0 +1,76 @@
+import React, { useContext, useState, useRef, useEffect } from 'react';
+
+import { AppContext } from '../../contexts/AppContext/AppContext';
+
+import Modal from './Modal';
+import Input from '../input/Input';
+import Button from '../button/Button';
+
+const UserInfoModal = ({ id, title, onSubmit, initialVisibility }) => {
+  const { myName, setMyName } = useContext(AppContext);
+
+  const [name, setName] = useState(myName);
+
+  useEffect(() => {
+    document.querySelector(`#${id} input`).focus();
+  });
+
+  const handleInputChange = (e) => {
+    let value = e.target.value;
+
+    setName(value);
+  };
+
+  const handleKeyDown= (e) => {
+    if (e.keyCode === 13) {
+      //e.preventDefault();
+      submitName();
+    }
+  };
+
+  const submitName = () => {
+    setMyName(name);
+    
+    onSubmit();
+
+    document.getElementById(id).click();
+  };
+
+
+  return (
+    <Modal
+      id={ id }
+      title={ title }
+      initialVisibility={ initialVisibility }
+    >
+      <div className="flex flex-col px-5 pb-5">
+
+        <p className="pb-6 text-xl">
+          Set your name:
+        </p>
+
+        <div>
+          <Input
+            classNames="w-2/4 text-xl focus:border-primary mb-3 text-center"
+            type="text"
+            maxLength="20"
+            placeholder={ myName }
+            onChange={ handleInputChange }
+            onKeyDown={ handleKeyDown }
+            autoFocus
+          />
+
+          <Button
+            onClick={ submitName }
+            classNames='h-10 text-xl bg-primary hover:bg-primary-dark'
+          >
+            Continue
+          </Button>
+        </div>
+        
+      </div>
+    </Modal>
+  );
+};
+
+export default UserInfoModal;

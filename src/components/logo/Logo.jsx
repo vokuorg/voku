750ccb23698687957bd11c84baa4e0cab711976d
jokenox <jokenox@gmail.com>
2021-11-03 04:22:22 -0600

    Add link to logo component

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { ReactComponent as Isotype } from '../../assets/logo.svg';
 
 import '../../index.css';
-import './Logo.css';
 
-function Logo({ textColor }) {
+function Logo({ textColor, linkTo = '#' }) {
   return (
     <div className={ textColor === 'black' ? 'text-black' : 'text-white' }>
-      <Isotype className="inline-block w-10 h-10 mr-3" />
-      <span className="inline-block text-4xl tracking-wide align-middle alata-font">videovoko</span>
+      <Link to={ linkTo }>
+        <Isotype className="inline-block w-10 h-10 mr-3" />
+        <span className="inline-block text-4xl tracking-wide align-middle alata-font">videovoko</span>
+      </Link>
     </div>
   );
 }

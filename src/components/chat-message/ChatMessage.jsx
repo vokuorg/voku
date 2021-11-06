d9d9696d5d7441f5ddd88a1e2ecee6db08443309
jokenox <jokenox@gmail.com>
2021-11-05 23:01:03 -0600

    Add Linkify to ChatMessage

--- src/components/chat-message/ChatMessage.jsx
+++ src/components/chat-message/ChatMessage.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import Linkify from 'react-linkify';
 
 import '../../index.css';
 import './ChatMessage.css';
 
 function ChatMessage({ text, side, classNames }) {
+  const componentDecorator = (href, text, key) => (
+    <a
+      href={ href }
+      key={ key }
+      target="_blank"
+      className="font-bold text-secondary-dark"
+    >
+      {text}
+    </a>
+  );
+
   return (
     <div className={ side === 'left' ? 'pr-6' : 'pl-6' }>
       <div className={ `message ${side} ${classNames || ''}` }>
-        { text }
+        <Linkify componentDecorator={ componentDecorator }>
+          { text }
+        </Linkify>
       </div>
     </div>
   );

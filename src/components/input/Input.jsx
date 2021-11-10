8e21d1d72ded23db16b5a60f585df475c5b7d7ae
jokenox <jokenox@gmail.com>
2021-11-09 23:59:34 -0600

    Remove margin right of Input component

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({ type, value, classNames, placeholder, onChange, onKeyDown, maxL
   return(
     <input
       type={ type }
-      className={`${classNames || ''} h-10 px-4 py-1 mr-2 overflow-auto border-2 border-solid rounded-lg resize-none border-gray bg-dark-secondary focus:outline-none`}
+      className={`${classNames || ''} h-10 px-4 py-1 overflow-auto border-2 border-solid rounded-lg resize-none border-gray bg-dark-secondary focus:outline-none`}
       value={ value }
       onChange={ onChange }
       onKeyDown={ onKeyDown }
